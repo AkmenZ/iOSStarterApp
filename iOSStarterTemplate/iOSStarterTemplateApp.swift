99fea2717080c2d2d9d0153413001b3dde292aaf
AkmenZ <akmenz89@gmail.com>
2024-01-21 21:31:42 +0000

    Initial Commit

---
+++ iOSStarterTemplate/iOSStarterTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSStarterTemplateApp.swift
+//  iOSStarterTemplate
+//
+//  Created by Janis Akmentins on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSStarterTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
